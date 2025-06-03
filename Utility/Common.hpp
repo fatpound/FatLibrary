86892b70e5086203ea2de9f01ad3a993c3f1392d
fatpound <84704675+fatpound@users.noreply.github.com>
2025-06-03 18:28:26 +0300

    sync

--- Utility/Common.hpp
+++ Utility/Common.hpp
@@ -5,9 +5,13 @@
 #endif
 
 #include <cstdint>
+#include <cstdlib>
+#include <cmath>
 
+#include <algorithm>
 #include <string>
 #include <string_view>
+#include <concepts>
 
 namespace fatpound::utility
 {
@@ -48,5 +52,33 @@ namespace fatpound::utility
             HexDigits[static_cast<unsigned int>(value >> 4U) bitand 0x0FU],
             HexDigits[value bitand 0x0FU]
         };
+    }
+
+    template <std::floating_point FP = double, std::integral T>
+    static
+    CX_MATH23
+    auto ClockHandsAngle(T hours, T minutes) noexcept -> FP
+    {
+        if constexpr (std::signed_integral<T>)
+        {
+            hours   = std::abs(hours);
+            minutes = std::abs(minutes);
+        }
+
+        hours   %= 12;
+        minutes %= 60;
+
+        const auto angle = std::fabs(static_cast<FP>((11 * minutes) - (60 * hours)) / static_cast<FP>(2.0));
+
+        return (angle > 180) ? static_cast<FP>(360 - angle) : angle;
+    }
+    
+    constexpr auto StrRev(const std::string& str) -> std::string
+    {
+        auto revstr = str;
+
+        std::ranges::reverse(revstr);
+
+        return revstr;
     }
 }
--- Utility/Utility.hpp
+++ Utility/Utility.hpp
@@ -3,46 +3,9 @@
 #include <FatSTL_Macros.hpp>
 
 #include <Utility/Gfx/Gfx.hpp>
+
 #include <Utility/Common.hpp>
 #include <Utility/Timer.hpp>
 #include <Utility/Color.hpp>
 #include <Utility/Surface.hpp>
 #include <Utility/ViewXM.hpp>
-
-#include <cstdlib>
-#include <cmath>
-
-#include <algorithm>
-#include <string>
-#include <concepts>
-
-namespace fatpound::utility
-{
-    template <std::floating_point FP = double, std::integral T>
-    static
-    CX_MATH23
-    auto ClockHandsAngle(T hours, T minutes) noexcept -> FP
-    {
-        if constexpr (std::signed_integral<T>)
-        {
-            hours   = std::abs(hours);
-            minutes = std::abs(minutes);
-        }
-
-        hours   %= 12;
-        minutes %= 60;
-
-        const auto angle = std::fabs(static_cast<FP>((11 * minutes) - (60 * hours)) / static_cast<FP>(2.0));
-
-        return (angle > 180) ? static_cast<FP>(360 - angle) : angle;
-    }
-    
-    constexpr auto StrRev(const std::string& str) -> std::string
-    {
-        auto revstr = str;
-
-        std::ranges::reverse(revstr);
-
-        return revstr;
-    }
-}
